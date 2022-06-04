1f81fa0f82df17796045b6ffbcfe2420b6e55ec9
yusufishakk <ysfishak14@gmail.com>
2022-06-04 14:06:24 +0300

    overloading with method

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+        public static void main(String[] args){
+            Dortislem dortislem= new Dortislem();
+
+            System.out.println(dortislem.topla(2,3)); // topla method ismiyle 2 adet sayı toplandı
+            System.out.println(dortislem.topla(2,3,4)); // topla method ismiyle 3 adet sayı toplandı
+        }
+    }
+
